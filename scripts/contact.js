0609db159fcb11835110584da13db62bd4cf8c21
Neeraj <135381896+keq-10@users.noreply.github.com>
2024-04-16 22:45:32 -0600

    Update contact.js

--- scripts/contact.js
+++ scripts/contact.js
@@ -1,3 +1,9 @@
+/***
+Author : Neeraj Bachwani
+Version: 16th April 2024
+Description : Java script added to contact page of delilah dog care 
+***/
+
 // when the "submit-button" is clicked, the contents of the contact-page are replaced with a single <p> element that reads "Thank you for your message" in size 24 font.
 
 
